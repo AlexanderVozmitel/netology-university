b9ab95a11ab0344cdf61b77f0768b0ffe88dcf54
Alexander_Vozmitel <avozmitel@mail.ru>
2017-01-19 18:34:02 +0300

    V1.1

--- 1.2/index.php
+++ 1.2/index.php
@@ -2,24 +2,28 @@
 	error_reporting(E_ALL);
 	ini_set('display_errors', 1);
 
-	$x = intval($_GET['number']);
-	$y = 10;
+	$x = 1;
+	$y = 1;
+	$z = intval($_GET['number']);
 
 	while (true)
 	{
-		if ($y > $x)
+		echo $y." - ".$x."<br>";
+		if ($x > $z)
 		{
 			echo 'задуманное число НЕ входит в числовой ряд';
 			break;
 		}
-		if ($y === $x)
-		{
+		if ($x === $z)
+		{	
+			echo $y." ".$x;
 			echo 'задуманное число входит в числовой ряд';
 			break;
+		}else {
+			$z = $x;
+			$x += $y;
+			$y = $z;
 		}
-		$z = $x;
-		$x = $x + $y;
-		$y = $z;
 	}
 
 ?>

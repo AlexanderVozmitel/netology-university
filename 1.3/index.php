d96e1faa6b64ff97d54c836eb8b61e1adfb1d65a
Alexander_Vozmitel <avozmitel@mail.ru>
2017-01-25 22:28:57 +0300

    v1.2

--- 1.3/index.php
+++ 1.3/index.php
@@ -5,47 +5,63 @@
 	$continent = array(
 		'Africa' => array(
 			'Mammuthus columbi',
+			'Scorpiones',
 			'Gazella dorcas',
+			'Colobus',
 			'Felis margarita',
-			'Trioceros jacksonii'
+			'Trioceros jacksonii',
+			'Theropithecus gelada'
 		),
 		'Australia' => array(
 			'Dromaius novaehollandiae',
+			'Trichosurus',
 			'Sarcophilus laniarius',
 			'Thylacinus cynocephalus',
-			'Cygnus atratus'
+			'Tachyglossidae',
+			'Cygnus atratus',
+			'Vombatidae'
 		)
 	);
 	
 	$array = array();
+	$random = array();
+	$quantity = 0;
 	
 	foreach ($continent['Africa'] as $Value) {
-		$temp = explode(' ', $Value);
-		$array[0][] = $temp[0];
-		$array[1][] = $temp[1];
-		unset($temp);
+		if (str_word_count($Value) == 2) {
+			$temp = explode(' ', $Value);
+			$array[0][] = $temp[0];
+			$array[1][] = $temp[1];
+			unset($temp);
+			$quantity++;
+		}
 	}
 	foreach ($continent['Australia'] as $Value) {
-		$temp = explode(' ', $Value);
-		$array[0][] = $temp[0];
-		$array[1][] = $temp[1];
-		unset($temp);
+		if (str_word_count($Value) === 2) {
+			$temp = explode(' ', $Value);
+			$array[0][] = $temp[0];
+			$array[1][] = $temp[1];
+			unset($temp);
+			$quantity++;
+		}
 	}
 	
-	$random = array(
-		0 => array(0,1,2,3,4,5,6,7),
-		1 => array(7,6,5,4,3,2,1,0)
-	);
-	
-	shuffle($random[0]);
-	shuffle($random[1]);
+	$i = 0;
+	while( $i <= ($quantity - 1) ){
+		$random[] = $i;
+		$i++;
+	}
+
+	shuffle($random);
 	
 	foreach ($array[0] as $Key => $Value) {
-		$result[] = $array[0][$random[0][$Key]];
+		$result[] = $array[0][$random[$Key]];
 	}
 	
+	shuffle($random);
+	
 	foreach ($array[1] as $Key => $Value) {
-		$result[$Key] .= ' '.$array[1][$random[1][$Key]];
+		$result[$Key] .= ' '.$array[1][$random[$Key]];
 	}
 	
 	foreach ($result as $Key => $Value) {
